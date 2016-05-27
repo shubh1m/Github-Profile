4e0e0e9e29e38ede6565c849de99d5a1b7989bbf
iamshubh22 <sgshubham03@gmail.com>
2016-05-27 19:10:14 +0530

    Create view

--- Github/views.py
+++ Github/views.py
@@ -1,40 +1,37 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
-import urllib.request
+import urllib.request, json
+import requests
 from Github.forms import SearchForm
 
-'''
-def home(request):
-    form = SearchForm()
-    print('in home')
-    return render(request, 'index.html', {'form': form})
-'''
-
 def index(request):
-    url = 'https://api.github.com/users/'
+    BASE_URL = 'https://api.github.com/users/'
     form = SearchForm(request.POST or None)
-    name = ''
+    name = None
+    details = None
 
     if request.method == 'POST':
-        name = request.POST.get('search')
+        try:
+            name = request.POST.get('search')
+
+            #print(request.POST)
+            url = BASE_URL + name
+            r = requests.get(url)
+            #print(r.status_code)
+            #if(r.status_code == 200)
+            details = r.json()
+        except ConnectionError:
+            print("Connection Error!! Try again later")
+        except HTTPError:
+            print("Invalid HTTP Response")
+        except:
+            print('Error in try block')
 
     context = {
         'form': form ,
         'name': name ,
+        'details': details,
     }
     return render(request, 'index.html', context)
-
-'''
-def Search(request):
-    url = 'https://api.github.com/users/'
-
-    if request.method == 'POST':
-        name = request.POST.get('search')
-        return HttpResponse('thanks')
-        #return render_to_response('Github/index.html', {'name': name}, RequestContext(request))
-        #return render(request, 'Github/index.html', {'name': name})
-    else:
-        form = SearchForm()
-    return render(request, 'index.html', {'form': form, 'name': name})
-'''
+    
